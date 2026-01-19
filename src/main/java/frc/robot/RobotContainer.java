22a12e02499b29e076113442311fcdbbcd43101f
JamesGames7 <crask26@pickeringcollege.on.ca>
2026-01-19 15:24:42 -0500

    Added vision subsystem (photon vision)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,38 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.DriveJoystickCommand;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
+	private SwerveSubsystem m_SwerveSubsystem;
+	private LEDSubsystem m_LedSubsystem;
+	private VisionSubsystem m_VisionSubsystem;
 
-  private void configureBindings() {}
+	private CommandXboxController driveController = new CommandXboxController(Constants.ControllerConstants.kDriverController);
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+	public RobotContainer() {
+		m_SwerveSubsystem = new SwerveSubsystem();
+		m_LedSubsystem = new LEDSubsystem();
+		m_VisionSubsystem = new VisionSubsystem(m_LedSubsystem);
+
+		configureBindings();
+	}
+
+	private void configureBindings() {
+		m_SwerveSubsystem.setDefaultCommand(new DriveJoystickCommand(
+			m_SwerveSubsystem, 
+			() -> driveController.getRawAxis(Constants.ControllerConstants.kDriverYAxis), 
+			() -> driveController.getRawAxis(Constants.ControllerConstants.kDriverXAxis), 
+			() -> driveController.getRawAxis(Constants.ControllerConstants.kDriverRotAxis), 
+			() -> driveController.button(Constants.ControllerConstants.kDriverFieldOrientedButtonId).getAsBoolean(), 
+			() -> driveController.getRawAxis(Constants.ControllerConstants.kDriverSlowAxis)));
+	}
+
+	public Command getAutonomousCommand() {
+		return Commands.print("No autonomous command configured");
+	}
 }
---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+public class VisionSubsystem extends SubsystemBase {
+	/** Creates a new VisionSubsystem. */
+	private final PhotonCamera camera = new PhotonCamera("MainCamera");
+	private PhotonPipelineResult result;
+	private LEDSubsystem m_LedSubsystem;
+
+	public VisionSubsystem(LEDSubsystem ledSubsystem) {
+		m_LedSubsystem = ledSubsystem;
+	}
+
+	public boolean hasTargets() {
+		return result.hasTargets();
+	}
+
+	public List<PhotonTrackedTarget> getTargets() {
+		if (result.hasTargets()) {
+			return result.getTargets();
+		}
+		return new ArrayList<>();
+	}
+
+	public PhotonTrackedTarget getSpecificTarget(int id) {
+		for (PhotonTrackedTarget target : getTargets()) {
+			if (target.getFiducialId() == id) return target;
+		}
+		return null;
+	}
+
+	public Transform3d getPoseToTarget(int id) {
+		PhotonTrackedTarget target = getSpecificTarget(id);
+		if (target != null) {
+			Transform3d distance = target.bestCameraToTarget;
+			return distance;
+		}
+		return null;
+	}
+
+	@Override
+	public void periodic() {
+		// This method will be called once per scheduler run
+		List<PhotonPipelineResult> unread = camera.getAllUnreadResults();
+		if (unread.size() > 0) result = unread.get(unread.size() - 1);
+		CommandScheduler.getInstance().schedule(m_LedSubsystem.setColour(result.hasTargets() ? 0 : 255, result.hasTargets() ? 255 : 0, 0)	);
+	}
+}
